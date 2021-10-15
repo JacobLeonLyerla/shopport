d449471933de9125d413d07f5e30bdee1faaa2f3
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-15 02:15:58 -0700

    started homepage component

---
+++ shopport/src/homepage.components.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            HAT
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+           JACKETS
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            SNEAKERS
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            WOMENS
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            MENS
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">
+            HAT
+            <span className="subtitle">SHOP NOW</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  </div>
+);
