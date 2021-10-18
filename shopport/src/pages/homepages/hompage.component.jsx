cf45d77f760ff3fdd0757f98959c6871139a9d45
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-18 00:14:21 -0700

    removed hard coded jsx and rendered my directory instead

--- shopport/src/pages/homepages/hompage.component.jsx
+++ shopport/src/pages/homepages/hompage.component.jsx
@@ -1,35 +1,12 @@
 import React from "react";
 import "./hompage.styles.scss";
+import Directory from "../../components/directory/directory.component"
 
 const HomePage = () => (
   <div className="homepage">
     
   
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">JACKETS </h1>
-
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">SNEAKERS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">WOMENS </h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title">MENS</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
-      </div>
+      <Directory/>
     </div>
   
 );
