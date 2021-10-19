1006e3a25327182e0fb03eb3687f0654cfc57c06
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-18 22:13:01 -0700

    removed hats component

--- shopport/src/App.js
+++ shopport/src/App.js
@@ -3,18 +3,15 @@ import React from "react";
 import { Switch, Route } from "react-router";
 
 import "./App.css";
-
+import ShopPage from "./pages/shop/shop.component.jsx";
 import HomePage from "./pages/homepages/hompage.component.jsx";
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route  path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
