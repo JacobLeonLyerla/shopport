20c7def99c8d8bd72c8122135d10ee9361ed5f06
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-18 23:54:24 -0700

    minor changed and added in my logo svg

--- shopport/src/App.js
+++ shopport/src/App.js
@@ -3,12 +3,15 @@ import React from "react";
 import { Switch, Route } from "react-router";
 
 import "./App.css";
+
+import Header from "./components/header/header.component";
 import ShopPage from "./pages/shop/shop.component.jsx";
 import HomePage from "./pages/homepages/hompage.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route  path="/shop" component={ShopPage} />
--- shopport/src/components/collection-preview/collection-preview.component.jsx
+++ shopport/src/components/collection-preview/collection-preview.component.jsx
@@ -3,6 +3,7 @@ import CollectionItem from "../collection-item/collection-item.component";
 import "./collection-preview.styles.scss";
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
+    <h1 className="title">{title}</h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
