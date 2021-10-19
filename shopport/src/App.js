14caeef3255975da89fe1835ecaba741ed2c3469
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-18 17:20:39 -0700

    Started building out router in app.js

--- shopport/src/App.js
+++ shopport/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import './App.css';
-import HomePage from './pages/homepages/hompage.component.jsx';
+import React from "react";
+
+import { Switch, Route } from "react-router";
+
+import "./App.css";
+
+import HomePage from "./pages/homepages/hompage.component.jsx";
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 function App() {
   return (
-    <div >
-     <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
