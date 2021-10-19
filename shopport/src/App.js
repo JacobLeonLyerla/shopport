0b7d812be6903d72b9d71485dffb7cbdf1076368
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-19 13:43:49 -0700

    imported in and routed my sign in component.

--- shopport/src/App.js
+++ shopport/src/App.js
@@ -2,19 +2,22 @@ import React from "react";
 
 import { Switch, Route } from "react-router";
 
-import "./App.css";
 
 import Header from "./components/header/header.component";
-import ShopPage from "./pages/shop/shop.component.jsx";
-import HomePage from "./pages/homepages/hompage.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import HomePage from "./pages/homepages/hompage.component";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route  path="/shop" component={ShopPage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
+
       </Switch>
     </div>
   );
