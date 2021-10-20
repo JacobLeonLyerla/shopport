6daeddd8787ba35d526220bc79af8084c59b49cd
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-19 19:04:38 -0700

    created my firebase utilities file

--- shopport/src/firebase/firebase.utils.js
+++ shopport/src/firebase/firebase.utils.js
@@ -11,6 +11,29 @@ const config = {
   appId: "1:69729263371:web:fd175a6fa6a6c1faeacc0e",
   measurementId: "G-P6H2JXWW51",
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user");
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
