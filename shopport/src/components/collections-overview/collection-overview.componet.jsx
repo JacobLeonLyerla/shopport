19e4711b80f5ccf72f180e6275af999543a0ac6f
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-25 00:40:00 -0700

    Created our collection overview component and styled it
    our collection overview allows us to render out our collection previews by passing in the data from our collection for each item we need to render

---
+++ shopport/src/components/collections-overview/collection-overview.componet.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCollectionForPreview } from "../../redux/shop/shop.selector";
+
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+import "./collection-overview.styles.scss";
+const CollectionOverview = ({ collections }) => {
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...oterCollectionProps }) => (
+        <CollectionPreview
+          key={oterCollectionProps.id}
+          {...oterCollectionProps}
+        />
+      ))}
+    </div>
+  );
+};
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollectionForPreview,
+});
+
+export default connect(mapStateToProps)(CollectionOverview);
