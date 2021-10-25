25cd60f312396b89433c8f9886c42de8327b79e3
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-24 21:38:09 -0700

    connected persist into our state

--- shopport/src/index.js
+++ shopport/src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { persistGate } from "redux-persist";
+import {store, persistor} from "./redux/store";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -11,7 +12,9 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <persistGate persistor={persistor}>
+          <App />
+        </persistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
