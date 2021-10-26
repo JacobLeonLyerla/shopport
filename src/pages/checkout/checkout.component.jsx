cf1113bc65d1f8821a972009ac481ee51d889ef5
Jacob Leon Lyerla <umasour@yahoo.com>
2021-10-25 17:01:45 -0700

    added a code to stop rendering most of the checkout component and instead show an alert telling you that your cart is empty

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -9,8 +9,8 @@ import CheckoutItem from "../../components/checkout-item/checkout-item.component
 import StripCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import "./checkout.styles.scss";
-const CheckoutPage = ({cartItems,total}) => {
-  return (
+const CheckoutPage = ({ cartItems, total }) => {
+  return total ? (
     <div className="checkout-page">
       <div className="checkout-header">
         <div className="header-block">
@@ -29,22 +29,36 @@ const CheckoutPage = ({cartItems,total}) => {
           <span>Remove</span>
         </div>
       </div>
-      {cartItems.map(cartItem =>
-        <CheckoutItem key={cartItem.id} cartItem={cartItem}/>)}
-        <div className="total"><span>total:${total}</span></div>
-        <div className="alert">
-          <span className="warning-text">*Please use the following test credit card for payments*</span>
-         <span className="warning-text"> Number: 4242 4242 4242 4242, Exp: 12/22, CVV: 123.</span>
-</div>
-        
-        <StripCheckoutButton  price={total}/>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      <div className="total">
+        <span>total:${total}</span>
+      </div>
+      <div className="alert">
+        <span className="warning-text">
+          *Please use the following test credit card for payments*
+        </span>
+        <span className="warning-text">
+          {" "}
+          Number: 4242 4242 4242 4242, Exp: 12/22, CVV: 123.
+        </span>
+      </div>
+
+      <StripCheckoutButton price={total} />
+    </div>
+  ) : (
+    <div className="checkout-page">
+      <div className="alert">
+        <span className="warning-text">You have no items in your cart</span>
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
-    cartItems :selectCartItems,
-    total: selectCartTotal
-})
+  cartItems: selectCartItems,
+  total: selectCartTotal,
+});
 
 export default connect(mapStateToProps)(CheckoutPage);
